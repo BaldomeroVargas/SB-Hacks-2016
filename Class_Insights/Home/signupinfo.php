0bbfc76716b266ae44af7ecd56a16e5f3d80dff0
Henry <hdoan002@ucr.edu>
2016-04-23 11:24:47 -0700

    getting inputs to passthrough

--- Class_Insights/Home/signupinfo.php
+++ Class_Insights/Home/signupinfo.php
@@ -1,7 +1,16 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["firstname"]; ?><br>
+  Welcome
+  <?php
+     echo $_POST["firstname"];
+     echo " ";
+     echo $_POST["lastname"];
+     echo "<br>You're username is : ";
+     echo $_POST["username"]; 
+     echo "<br>You're password is : ";
+     echo $_POST["password"];
+  ?> 
 
 </body>
 </html>
