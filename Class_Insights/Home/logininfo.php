1e987583676560d7672d1e2bda8d94c78df82eb9
schoi044 <schoi044@ucr.edu>
2016-04-24 12:59:48 +0000

    Made major UI changes to the entirety of the program.

--- Class_Insights/Home/logininfo.php
+++ Class_Insights/Home/logininfo.php
@@ -3,16 +3,20 @@
 <?php
 include("users.php");
 
-function check_user() {
+function check_user()
+{
 	$user = new Users();
 	return $user->check($_POST['username'], $_POST['password']);
 }
 
-if(check_user()){
+if(check_user())
+{
 	echo "Successful login!";
+	header("Location: ../index.html")
 }
 else{
 	echo "Error: Username or password is incorrect";
+	header("Location: login.html")
 }
 
 ?>
